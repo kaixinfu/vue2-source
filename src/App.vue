b3c4e7099644bafe6136767d0b0dfaf910a01ea4
liu kaixin <1360517661@qq.com>
2020-05-07 15:40:01 +0800

    create test.html

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,79 @@
 <template>
   <div id="app">
-    <div>
-      <router-link to="/">BBBB</router-link> |
-      <router-link to="/C">CCCC</router-link>
+    <input
+      @change="fileChange($event)"
+      type="file"
+      accept="image/*"
+    > //上传图片类型
+    <div class="add">
+      <input
+        @change="fileChange($event)"
+        type="file"
+        accept="image/*"
+      >
+      <div class="add-image">
+        <div class="add-ico">
+          <!-- <img src="../../../static/info3.png" alt=""> -->
+        </div>
+        <img
+          class="add-img"
+          v-show="imgL"
+          :src="imgL"
+          alt=""
+        >
+      </div>
+      <p class="font13">点击上传名片</p>
     </div>
-    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import A from './components/tongxun/A.vue'
+import A from "./components/tongxun/A.vue";
+import { fileAdd } from "./a.js";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      imgL: false
+    };
+  },
   components: {
     A
   },
   beforeCreate() {
-    console.log('APP ........ beforeCreate')
+    // console.log("APP ........ beforeCreate");
   },
-  mounted() {
-
+  mounted() {},
+  methods: {
+    //判断是否图片类型
+    fileChange(el) {
+      // console.log(el.target.files[0].size)
+      if (!el.target.files[0].size) {
+        this.$msgbox("请选择图片文件");
+        return;
+      } else {
+        this.fileList(el.target);
+        el.target.value = "";
+      }
+    },
+    //判断是否为文件夹文件
+    fileList(fileList) {
+      let files = fileList.files;
+      //判断是否为文件夹
+      if (files.type != "") {
+        fileAdd(files[0]);
+      } else {
+        this.$msgbox("请选择图片文件");
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /*text-align: center;*/
